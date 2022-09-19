1edca213a4b7b2695fb1b296da2daa58df568495
Kuh <73922679+ramadhankukuh@users.noreply.github.com>
2022-09-19 17:03:57 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Main'
+
+class App extends React.Component{
+  render (){
+    return (
+      <div className="bg-gradient-to-r from-indigo-100">
+        <Main/>
+      </div>
+    )
+  }
+}
+
+export default App;
